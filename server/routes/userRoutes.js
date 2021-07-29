1a2798e58f333b67336f43883ba1e4322cb29d40
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-07-29 08:28:39 -0500

    add conspirator routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -39,6 +39,44 @@ userRouter.get('/get/favorites', (req, res) => {
 .catch(err => console.log(err));
 }});
 
+// co-conspirators routes
+
+userRouter.post('/add/conspirator', async (req, res) => {
+  const { friendId } = req.body;
+  // console.log(id, nasa_id);
+  await User.findOne({ friendId })
+    .then(results => { console.log('RESULTS OBJ', results); return results; } )
+    .then((data) => User.findOneAndUpdate({ _id: req.user.id }, { $push: { coConspirators: data }}, { new: true }))
+    .catch(err => console.log('Evidence Error', err));
+});
+
+userRouter.get('/get/conspirators', (req, res) => {
+  if(req.user){
+  User.findById(req.user._id)
+  .then(user => {
+    const conspirators = user.coConspirators.map( objId => User.findOne({_id: objId }).exec());
+    return Promise.all(conspirators);
+  }).then((conspirators) => {
+  res.status(200).send(conspirators);
+})
+.catch(err => console.log(err));
+}});
+
+userRouter.post('/conspirator/favorites', (req, res) => {
+  if(req.user){
+  const { friendId } = req.body;
+  User.findById(friendId)
+  .then(user => {
+    const favorites = user.favorites.map( objId => Evidence.findOne({_id: objId }).exec());
+    return Promise.all(favorites);
+  }).then((favorites) => {
+  res.status(200).send(favorites.reverse());
+})
+.catch(err => console.log(err));
+}});
+
+
+
 
 
 module.exports = {
